925fe374d1ae8de89bb3d7f254ceddc08d341d4c
hassaanjalal <104089220+hassaanjalal@users.noreply.github.com>
2022-04-28 00:37:53 +0500

    Update readme.py

--- readme.py
+++ readme.py
@@ -1,15 +1,10 @@
-#This code gives nth number in fibonacci series - {0,1,1,2,3,5,......}.
-def fibonacci(n, second_last, last):
-	if n-1 == 0:
-		return second_last
-	else:
-		new_last = second_last + last
-		second_last = last
-		return fibonacci(n-1, second_last, new_last)
-
-
-if __name__ == "__main__":
-	print(fibonacci(10, 0, 1))
-
-# This code is contributed by Hassan Jalal
-
+a=int(input("Enter the first number of the series "))
+b=int(input("Enter the second number of the series "))
+n=int(input("Enter the number of terms needed "))
+print(a,b,end=" ")
+while(n-2):
+    c=a+b
+    a=b
+    b=c
+    print(c,end=" ")
+    n=n-1
